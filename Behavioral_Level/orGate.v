ee073a437faf61ca9bbc7cee6ef1f0330f3c63c6
Arjun Das <darjun650@gmail.com>
2024-03-13 15:28:14 +0530

    dkj

---
+++ Behavioral_Level/orGate.v
@@ -0,0 +1,28 @@
+module or_gate(A, B, C);
+input A, B; 
+output C; 
+reg C; 
+always @(A or B)
+begin 
+    if(A == 1'b0 && B == 1'b0)
+        C= 1'b0;
+    else
+        C= 1'b1;
+end 
+endmodule
+
+module test;
+reg A, B;
+wire C;
+or_gate i(A, B, C);
+initial
+    begin
+    A = 1'b0;
+    B = 1'b0; 
+    $monitor ("Time= %0t , A= %b , B = %b , C = %b" , $time , A , B , C); 
+    #5 A= 1'b0 ; B= 1'b0; 
+    #5 A= 1'b0 ; B= 1'b1; 
+    #5 A= 1'b1 ; B= 1'b0; 
+    #5 A= 1'b1 ; B= 1'b1; 
+end 
+endmodule
